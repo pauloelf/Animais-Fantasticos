48bdc0c477211fdcbffdf7d37b20c4eda75cae3f
Paulo Sérgio <97705403+pauloelf@users.noreply.github.com>
2022-11-07 01:40:01 +0000

    Adicionado Slide de fotos

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ import Funcionamento from './modules/funcionamento.js'
 import fetchAnimais from './modules/fetch-animais.js'
 import fetchBitcoin from './modules/fetch-bitcoin.js'
 import ScrollAnima from './modules/scroll-animacao.js'
+import SlideNav from './modules/slide.js'
 
 const scrollSuave = new ScrollSuave('.js-menu a[href^="#"]')
 scrollSuave.init()
@@ -40,3 +41,9 @@ funcionamento.init()
 fetchAnimais('./animaisapi.json', '.numeros-grid')
 
 fetchBitcoin('https://blockchain.info/ticker', '.btc-preco')
+
+
+const slide = new SlideNav('.slide', '.slide-display')
+
+slide.init()
+slide.addControl('.custom-controls')
